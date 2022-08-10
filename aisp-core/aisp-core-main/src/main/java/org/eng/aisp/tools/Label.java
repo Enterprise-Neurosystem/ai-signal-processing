2420f77174281ff94182e1676a1cd32d197c096a
David Wood <dawood@us.ibm.com>
2022-08-10 17:55:38 -0400

    Typo in help Label help text.

--- aisp-core/aisp-core-main/src/main/java/org/eng/aisp/tools/Label.java
+++ aisp-core/aisp-core-main/src/main/java/org/eng/aisp/tools/Label.java
@@ -47,7 +47,7 @@ public class Label {
 			  "Classifies sounds using a model to produce a metadata-formatted labeling of the\n"
 			+ "sounds on stdou.  While the -clipLen option is not strictly required, it is \n"
 			+ "typically used to classify sub-segments of the input sound(s).  Segments from\n"
-			+ "the same file that are are adjacent in time and have the same label name as \n" 
+			+ "the same file that are adjacent in time and have the same label name as \n" 
 			+ "produced by the model will be merged.\n"
 			+ "Classify mode options\n"
 			+ GetModifiedSoundOptions.ClipOnlyOptionsHelp
